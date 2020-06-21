d433632037c31884feb55f86788b1cb736c4ad8e
nuclearMonkeys <lyndond@uci.edu>
2020-06-21 12:35:12 -0700

    Added Untiy Project to git
    I used the GamePlusGames tutorial to learn how to make a FPS. Using some assets he provided, but all of it will be replaced. https://www.youtube.com/watch?v=Kgjth3nRsFc

---
+++ The-Quest-for-Booty/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    public float mouseSensitivity = 1f;
+
+    public Transfrom camTransform;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 x = transform.up * -moveInput.x;
+
+        Vector3 y = transform.right * moveInput.y;
+
+        rb.velocity = (x + y) * moveSpeed;
+
+        // player view control
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+            transform.rotation.eulerAngles.y,
+            transform.rotation.eulerAngles.z - mouseInput.x);
+
+        MainCamera.transform.localRotation = Quaternion.Euler(MainCamera.transform.localRotation);
+    }
+}
